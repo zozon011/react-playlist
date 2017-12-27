2913de5b6a3f1e80d868a9ba1f6423dfc252e41f
Zorica Janjic <zorica.j@gmail.com>
2017-12-27 07:22:37 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let defaultTextColor = "#fff";
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "40%", display: "inline-block" }}>
+        <h2>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class PlayList extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: "25%", display: "inline-block" }}>
+        <img />
+        <h3>Playlist Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div style={defaultStyle}>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <PlayList />
+        <PlayList />
+        <PlayList />
+      </div>
+    );
+  }
+}
+
+export default App;
