bb244dacde65a930987b4bbcf8c727e960b1817d
Zorica Janjic <zorica.j@gmail.com>
2018-01-01 11:16:53 +0100

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 let defaultTextColor = "#fff";
@@ -48,7 +47,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style={{ ...defaultStyle, "font-size": "50px" }}>Title</h1>
         <Aggregate />
         <Aggregate />
         <Filter />
